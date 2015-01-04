4a68c8c24caf68c9794a92498b7bed5923303b88
klord9x <tanhuan.klord@gmail.com>
2015-01-04 17:59:06 +0700

    WP3: 04 Building the Header

--- wp-content/themes/my-simone/header.php
+++ wp-content/themes/my-simone/header.php
@@ -21,11 +21,27 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
+	<header id="masthead" class="site-header" role="banner">            
+            <?php if ( get_header_image() && ('blank' == get_header_textcolor())) { ?>
+                <figure class="header-image">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+                    </a>
+                </figure>
+            <?php }; // End header image check. ?>
+            <?php
+                if (get_header_image() && !('blank' == get_header_textcolor())) {
+                    echo '<div class="site-branding header-background-image" style="background-image:url('.get_header_image().')">';
+                } else {
+                    echo '<div class="site-branding">';
+                }
+            ?>
+            
+                <div class="title-box">
+                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                    <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+                </div>
+                </div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h1 class="menu-toggle"><?php _e( 'Menu', 'my-simone' ); ?></h1>
