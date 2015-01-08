f71a2d79746201d6933fcadefc525720148fbb28
huan.nguyen <huan.nguyen@audiencemedia.com>
2015-01-08 18:19:28 +0700

    WP10: 11.7: 11 Working with Index Templates

--- wp-content/themes/my-simone/content.php
+++ wp-content/themes/my-simone/content.php
@@ -5,59 +5,46 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class =" index-box">
+        <?php
+        if (has_post_thumbnail()) {
+            echo '<div class="small-index-thumbnail clear">';
+            echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'my-simone') . get_the_title() . '" rel="bookmark">';
+            echo the_post_thumbnail('index-thumb');
+            echo '</a>';
+            echo '</div>';
+        }
+        ?>
 	<header class="entry-header">
+            <?php
+            // Display a thumb tack in the top right hand corner if this post is sticky
+            if (is_sticky()) {
+                echo '<i class="fa fa-thumb-tack sticky-post"></i>';
+            }
+            ?>
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php my_simone_posted_on(); ?>
+                        <?php 
+                            if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) { 
+                                echo '<span class="comments-link">';
+                                comments_popup_link( __( 'Leave a comment', 'my-simone' ), __( '1 Comment', 'my-simone' ), __( '% Comments', 'my-simone' ) );
+                                echo '</span>';
+                            }
+                        ?>
+                        <?php edit_post_link( __( 'Edit', 'my-simone' ), '<span class="edit-link">', '</span>' ); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
-
-	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-	<?php else : ?>
+		
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'my-simone' ) ); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'my-simone' ),
-				'after'  => '</div>',
-			) );
-		?>
+            <?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
-	<?php endif; ?>
 
-	<footer class="entry-footer">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'my-simone' ) );
-				if ( $categories_list && my_simone_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'my-simone' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'my-simone' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'my-simone' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'my-simone' ), __( '1 Comment', 'my-simone' ), __( '% Comments', 'my-simone' ) ); ?></span>
-		<?php endif; ?>
-
-		<?php edit_post_link( __( 'Edit', 'my-simone' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
+	<footer class="entry-footer continue-reading">
+            <?php echo '<a href="' . get_permalink() . '" title="' . __('Continue Reading ', 'my-simone') . get_the_title() . '" rel="bookmark">Continue Reading<i class="fa fa-arrow-circle-o-right"></i></a>'; ?>
+        </footer><!-- .entry-footer -->
+    </div><!-- .index-box--> 
 </article><!-- #post-## -->
